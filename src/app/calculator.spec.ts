c8a73ab80a0756ab71c2eb49b524b47ef8899bb8
Francisco del Collado <fedelcollado@gmail.com>
2024-02-06 17:02:06 -0600

    mocha report

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,59 +1,66 @@
 import { Calculator } from "./calculator";
 
 describe('Test for Calculator', () => {
-  it('#multiply should return a nine', () => {
-    //AAA
-    //Arrange
-    const calculator = new Calculator;
-    //Act
-    const rta = calculator.multiply(3,3);
-    //Assert
-    expect(rta).toEqual(9);
+
+  describe('Tests for multiply', () => {
+    it('Should return a nine', () => {
+      //AAA
+      //Arrange
+      const calculator = new Calculator;
+      //Act
+      const rta = calculator.multiply(3,3);
+      //Assert
+      expect(rta).toEqual(9);
+    });
+
+    it('Should return a four', () => {
+      //AAA
+      //Arrange
+      const calculator = new Calculator;
+      //Act
+      const rta = calculator.multiply(1, 4);
+      //Assert
+      expect(rta).toEqual(4);
+    });
   });
 
-  it('#multiply should return a four', () => {
-    //AAA
-    //Arrange
-    const calculator = new Calculator;
-    //Act
-    const rta = calculator.multiply(1, 4);
-    //Assert
-    expect(rta).toEqual(4);
+  describe('Tests for divide', ()=> {
+    it('Should return some numbers', () => {
+      //AAA
+      //Arrange
+      const calculator = new Calculator;
+      //Act and Assert
+      expect(calculator.divide(6,3)).toEqual(2);
+      expect(calculator.divide(5,2)).toEqual(2.5);
+    });
+
+    it('For a zero', () => {
+      //AAA
+      //Arrange
+      const calculator = new Calculator;
+      //Act and Assert
+      expect(calculator.divide(6,0)).toBeNull();
+      expect(calculator.divide(5,0)).toBeNull();
+      expect(calculator.divide(12345647, 0)).toBeNull();
+    });
   });
 
-  it('#divide should return some numbers', () => {
-    //AAA
-    //Arrange
-    const calculator = new Calculator;
-    //Act and Assert
-    expect(calculator.divide(6,3)).toEqual(2);
-    expect(calculator.divide(5,2)).toEqual(2.5);
-  });
+  describe('Tests for matchers', () => {
+    it('test matchers', () => {
+      let name = 'Isco';
+      let name2;
 
-  it('#divide for a zero', () => {
-    //AAA
-    //Arrange
-    const calculator = new Calculator;
-    //Act and Assert
-    expect(calculator.divide(6,0)).toBeNull();
-    expect(calculator.divide(5,0)).toBeNull();
-    expect(calculator.divide(12345647, 0)).toBeNull();
-  });
+      expect(name).toBeDefined();
+      expect(name2).toBeUndefined();
 
-  it('test matchers', () => {
-    let name = 'Isco';
-    let name2;
+      expect(1 + 3 === 4).toBeTruthy(); // 4
+      expect(1 + 1 === 3).toBeFalsy();
 
-    expect(name).toBeDefined();
-    expect(name2).toBeUndefined();
+      expect(5).toBeLessThan(10);
+      expect(20).toBeGreaterThan(10);
 
-    expect(1 + 3 === 4).toBeTruthy(); // 4
-    expect(1 + 1 === 3).toBeFalsy();
-
-    expect(5).toBeLessThan(10);
-    expect(20).toBeGreaterThan(10);
-
-    expect('123456').toMatch(/123/);
-    expect(['apples', 'oranges', 'pears']).toContain('oranges');
+      expect('123456').toMatch(/123/);
+      expect(['apples', 'oranges', 'pears']).toContain('oranges');
+    });
   });
 });
