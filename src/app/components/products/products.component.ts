5b823deff8fc17e92d900c1ef02d79eafda9fb22
Francisco del Collado <fedelcollado@gmail.com>
2024-02-07 18:18:50 -0600

    Product service http

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = []
+
+
+  constructor(
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productService.getAllSimple()
+    .subscribe(products => {
+      this.products = products;
+    })
+  }
 
 }
