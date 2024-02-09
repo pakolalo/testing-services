90d4da4923ac8d4bd32989f3b838571edd2b143a
Francisco del Collado <fedelcollado@gmail.com>
2024-02-09 13:27:54 -0600

    HttpClientTestingModule

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ProductService } from './product.service';
+import { Product } from '../models/product.model';
+import { environment } from '../../environments/environment';
 
-describe('ProductService', () => {
-  let service: ProductService;
+fdescribe('ProductService', () => {
+  let productService: ProductService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers: [
+        ProductService
+      ]
+    });
+    productService = TestBed.inject(ProductService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(productService).toBeTruthy();
+  });
+
+  describe('tests for getAllSimple', () => {
+
+    it('should return a product list', (doneFn) => {
+      // Arrange
+      const mockData: Product[] = [
+        {
+          id: '123',
+          title: 'title',
+          price: 12,
+          description: 'blabla',
+          category: {
+            id: 123,
+            name: 'as',
+          },
+          images: ['img', 'img']
+        }
+      ];
+      //Act
+      productService.getAllSimple()
+      .subscribe((data) => {
+        //Assert
+        expect(data.length).toEqual(mockData.length);
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+      // http config
+      const url = `${environment.API_URL}/products`
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
   });
 });
