6aad350a564220daa4132252dab6579d3bbe502f
Francisco del Collado <fedelcollado@gmail.com>
2024-02-12 15:10:13 -0600

    componetes con input

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonComponent } from './person.component';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from '../../models/person.model';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -23,17 +24,33 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <h3> with "Hola, PersonComponent"', () => {
+  it('should the name be "Isco"', () => {
+    component.person = new Person('Isco', 'del Collado', 33, 1.73, 67);
+    expect(component.person.name).toEqual('Isco');
+  })
+
+  it('should have <h3> with "Hola, {person.name}"', () => {
+    //Arrange
+    component.person = new Person('Isco', 'del Collado', 33, 1.73, 67);
+    const expectMsg = `Hola, ${component.person.name}`;
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3Element: HTMLElement = h3Debug.nativeElement;
-    expect(h3Element?.textContent).toEqual('Hola, PersonComponent')
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(h3Element?.textContent).toEqual(expectMsg)
   })
 
-  it('should have <p> with "Soy un parrafo"',() => {
+  it('should have <p> with "Mi altura es {person.height}"',() => {
+    //Arrange
+    component.person = new Person('Isco', 'del Collado', 33, 1.73, 67);
     const personDebug: DebugElement = fixture.debugElement;
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Soy un parrafo');
+    //Act
+    fixture.detectChanges();
+    //Assert
+    expect(pElement?.textContent).toContain(component.person.height);
   });
 });
