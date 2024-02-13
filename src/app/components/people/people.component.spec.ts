d6bd3b262cd888d365dea5264dea02e20c81cf6b
Francisco del Collado <fedelcollado@gmail.com>
2024-02-12 18:58:16 -0600

    pruebas con componente padre

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,17 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
+import { PersonComponent } from '../person/person.component';
+import { Person } from '../../models/person.model';
+import { By } from '@angular/platform-browser';
 
-describe('PeopleComponent', () => {
+fdescribe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PeopleComponent]
+      imports: [PeopleComponent, PersonComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,19 @@ describe('PeopleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list app-person components', () => {
+    //Arrange
+    component.people = [
+      new Person('Isco', 'del Collado', 33, 1.73, 80),
+      new Person('Valentina', 'Molina', 15, 1.54, 48),
+      new Person('Santiago', 'Perez', 24, 1.80, 85),
+    ];
+        //Act
+    fixture.detectChanges()
+    const debugEl = fixture.debugElement.queryAll(By.css('app-person'));
+    //Assert
+    expect(debugEl.length).toEqual(3)
   });
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PersonComponent } from '../person/person.component';
 import { Person } from '../../models/person.model';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [PersonComponent,],
+  imports: [PersonComponent, CommonModule],
   templateUrl: './people.component.html',
   styleUrl: './people.component.css'
 })
 export class PeopleComponent {
 
-  person: Person = new Person('Isco', 'del Collado', 33, 1.73, 80);
+  people: Person[] = [
+    new Person('Isco', 'del Collado', 33, 1.73, 80),
+    new Person('Valentina', 'Molina', 15, 1.54, 48),
+  ];
+  selectedPerson: Person | null = null;
 
+  choose(person: Person) {
+    this.selectedPerson = person;
+  }
 }
