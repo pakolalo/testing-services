9d9f74b0a3aa9c61934ae8ed7a1b1f15432525f8
Francisco del Collado <fedelcollado@gmail.com>
2024-02-12 19:12:27 -0600

    some changes

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -37,4 +37,26 @@ fdescribe('PeopleComponent', () => {
     //Assert
     expect(debugEl.length).toEqual(3)
   });
+
+  it('should raise selected event when clicked', () => {
+    //Arrange
+    const buttonDe = fixture.debugElement.query(By.css('app-person .btn-choose'));
+    //Act
+    buttonDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    //Assert
+    expect(component.selectedPerson).toEqual(component.people[0]);
+  });
+
+  it('should render the selected person', () => {
+    //Arrange
+    const buttonDe = fixture.debugElement.query(By.css('app-person .btn-choose'));
+    //Act
+    buttonDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    const liDe = fixture.debugElement.query(By.css('.selectedPerson ul > li'));
+    //Assert
+    expect(component.selectedPerson).toEqual(component.people[0]);
+    expect(liDe.nativeElement.textContent).toContain(component.selectedPerson?.name);
+  });
 });
